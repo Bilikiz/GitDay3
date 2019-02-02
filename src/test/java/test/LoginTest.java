4d293b48997fe16ba0b7557ec24124c0182c54bc
belkiz <test@gmail.com>
2019-02-01 21:31:36 -0500

    added new method

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -25,13 +25,24 @@ public class LoginTest {
 
     }
 
-    @Test.
+    @Test
     public void loginTest1(){
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
 
+    }
+
+    @Test
+    public void LogOutTest(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
 
     }
     @AfterMethod
